6f52bfc4a02584f21b6df9710f2bcb8d9deed05e
Dimentiy88 <bodartinov88@gmail.com>
2024-02-17 16:29:24 +0100

    js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// const js = "JawaScript is awesome".length;
+// console.log("js: ", js);
+
+// const JS = "JawaScript is awesome".toUpperCase();
+// console.log("JS: ", JS);
+
+// const userName = "Dima";
+// console.log("Username is", userName);
+
+// const message = "JawaScript is awesome";
+// alert(message);
+
+// const nam = "2" + "4" + 1;
+// console.log(nam);
+
+// const guestName = "Dima";
+// const roomNumber = 204;
+// const greeting = `Welcome ${guestName}, you room number is ${roomNumber} `;
+// console.log("greeting: ", greeting);
+
+// const guestName = "Dima";
+// const roomNumber = 204;
+// const greeting = `Welcome ${guestName}, you room number is ${roomNumber} `;
+// console.log("greeting: ", greeting.indexOf("y"));
+
+// const num1 = 5;
+// const num2 = 10;
+// const biggerNumber = num1 > num2 ? num1 : num2;
+// console.log(biggerNumber);
