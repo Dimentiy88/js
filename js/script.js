0368f76bbb1880270013f6ea00cb0762008b3774
Dimentiy88 <bodartinov88@gmail.com>
2024-02-18 19:23:56 +0100

    function

--- js/script.js
+++ js/script.js
@@ -27,3 +27,48 @@
 // const num2 = 10;
 // const biggerNumber = num1 > num2 ? num1 : num2;
 // console.log(biggerNumber);
+
+// ? -=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// const a = 21;
+// const b = a * 2;
+// console.log("b: ", b);
+
+// const age = prompt("Please tell me your age");
+// console.log("age: ", age);
+
+// const a = "42";
+// const b = Number(a);
+// console.log("a: ", a);
+// console.log("b: ", b);
+
+// const bank_balance = 400;
+// const price = 800;
+
+// if (price < bank_balance) {
+//   console.log("Купить телефон");
+// } else {
+//   console.log("Недостаточно средств");
+// }
+
+// const myArray = [50, 60, 70];
+// const myData = myArray[1];
+
+// console.log(myData);
+
+// function functionWithArgs(param1, param2) {
+//   console.log(param1, param2);
+// }
+// functionWithArgs(7 + 9);
+
+// function plusThree(num) {
+//   return num + 3;
+// }
+// const answer = plusThree(5);
+// console.log("answer: ", answer);
+
+// function timesFive(num) {
+//   return num * 5;
+// }
+
+// const answer = timesFive(5);
